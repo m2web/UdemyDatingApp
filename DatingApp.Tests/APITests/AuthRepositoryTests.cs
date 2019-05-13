9b6bf7ad02c3f9907ed7368187a2dbe3b3dcb9b4
Mark McFadden <m2web@yahoo.com>
2019-05-13 10:40:31 -0400

    Added a xUnit testing project and user validation.

---
+++ DatingApp.Tests/APITests/AuthRepositoryTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using DatingApp.API.Data;
+using Moq;
+using DatingApp.API.Models;
+
+namespace APITests
+{
+    public class AuthRepositoryTests
+    {
+        private readonly DatingApp.API.Data.IAuthRepository _apiData;
+
+        //TODO: get past the following error:
+        /*
+            Error Message: Castle.DynamicProxy.InvalidProxyConstructorArgumentsException : 
+            Can not instantiate proxy of class: DatingApp.API.Data.DataContext.
+            Could not find a constructor that would match given arguments:
+            Moq.Mock`1[Microsoft.EntityFrameworkCore.DbContext]
+
+            Note once that is done uncomment CreatePasswordHashTest and test
+        */
+        public AuthRepositoryTests(){
+            var dbContectMock = new Mock<Microsoft.EntityFrameworkCore.DbContext>();
+            var mock = new Mock<DataContext>(dbContectMock);
+            _apiData = new DatingApp.API.Data.AuthRepository(mock.Object);
+        }
+
+        // [Fact]
+        // public void CreatePasswordHashTest()
+        // {
+        //     var user = new User();
+        //     _apiData.Register(user, "testpassword");
+        // }
+    }
+}

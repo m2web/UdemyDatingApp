31dd9fd289fd4900460a6208fbc276300b2331ae
Mark McFadden <m2web@yahoo.com>
2019-05-15 05:55:11 -0400

    Removed value component and moved the values to the register component.

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit {  
+  @Input() valuesFromHome: any;
+
   model: any = {};
 
   constructor() { }

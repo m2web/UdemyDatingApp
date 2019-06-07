5352573f549924013f10be03494f4fe55fb1a820
Mark McFadden <m2web@yahoo.com>
2019-06-07 10:03:52 -0400

    Continued with pagination

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -51,9 +51,9 @@ namespace DatingApp.API.Data
             var users = _context.Users.Include(p => p.Photos)
                 .OrderByDescending(u => u.LastActive).AsQueryable();
 
-            // users = users.Where(u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Id != userParams.UserId);
 
-            users = users.Where(u => u.Gender == userParams.Gender);
+            //users = users.Where(u => u.Gender == userParams.Gender);
 
             if (userParams.Likers)
             {
